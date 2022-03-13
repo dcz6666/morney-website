63c8af46107f4d6d11f4c13e789e237cde3b0bc8
王文成 <wangwc@morefun.me>
2022-03-13 17:28:58 +0800

    deploy

---
+++ precache-manifest.87876b928cce613ef4265629f8969a05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa12cdc18aae0f6988b",
+    "url": "/morney-website/css/app.760e8a42.css"
+  },
+  {
+    "revision": "5b171e17a9751415917e1ec3fab0dec6",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "5aa12cdc18aae0f6988b",
+    "url": "/morney-website/js/app.d4ff9927.js"
+  },
+  {
+    "revision": "d36ad65b77a159524bbe",
+    "url": "/morney-website/js/chunk-vendors.b55381c5.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
